d66fecd43e8a35caa52efd9d96a0f30a2e4e325a
Logan Crewss <logancrewss@Blakes-MacBook-Air.local>
2017-07-25 15:26:33 -0600

    update

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  const baseURL = 'https://polar-mesa-16379.herokuapp.com/'
+
+  $.get(baseURL)
+    .then(function(data){
+      console.log(data)
+      for (var i = 0; i < data.length; i++) {
+        let liquor = data[i];
+
+        $('.liquor').append(`
+          <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title name">${liquor.name}</span>
+
+              <div class="type"</div>Type of Liquor: ${liquor.type}</a>
+              <br><br>
+              <div class="abv"</div>ABV: ${liquor.abv}</a>
+
+            </div>
+            <div class="card-action">
+              <a class="waves-effect waves-light btn">Remove From List</a>
+            </div>
+          </div>
+        </div>`)
+      }
+
+
+    })
+})
